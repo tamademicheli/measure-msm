027f2300ec96f8ecfda9d80b3fa17f2fdde2b083
lorenzodemicheli <lorenzo.demicheli@bbv.ch>
2018-08-21 11:56:43 +0200

    first checkin

---
+++ src/main/java/sandbox/repositories/MeasureMsmApplication.java
@@ -0,0 +1,12 @@
+package sandbox.repositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureMsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureMsmApplication.class, args);
+	}
+}
---
+++ src/test/java/sandbox/repositories/MeasureMsmApplicationTests.java
@@ -0,0 +1,55 @@
+package sandbox.repositories;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.text.html.Option;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
+
+// TODO implement embedded client,... and right assertions
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeasureMsmApplicationTests {
+
+    @Autowired
+    TemperatureMeasureRepository temperatureMeasureRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    private TemperatureMeasure getTemperatureMeasure() {
+        TemperatureMeasure temperatureMeasure = new TemperatureMeasure();
+        temperatureMeasure.setDeviceId("test");
+        temperatureMeasure.setMeasureTime(LocalDateTime.now());
+        temperatureMeasure.setLatitude("12345");
+        temperatureMeasure.setLongitude("23456");
+        temperatureMeasure.setCelsius(23.12);
+        return temperatureMeasure;
+    }
+
+    @Test
+    public void testSaveSuccessful() {
+        TemperatureMeasure temperatureMeasure = getTemperatureMeasure();
+        temperatureMeasureRepository.save(temperatureMeasure);
+
+        Optional<TemperatureMeasure> measure = temperatureMeasureRepository.findById(temperatureMeasure.getId());
+
+        Assert.assertTrue(measure.isPresent());
+
+
+    }
+
+
+}
